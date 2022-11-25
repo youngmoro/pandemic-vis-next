c225d27262561b10274a36c03184b7a940d3680e
youngmoro <morofujix@keio.jp>
2022-11-25 22:03:37 +0900

    add alpha

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -48,11 +48,11 @@ const GlobeDefault = () => {
     const diff = calDiff(date); //0-1017
     const red = Math.floor(255 - (diff / 1017) * 140);
     const blue = Math.floor(100 + (diff / 1017) * 80);
-    return rgb2hex([red, 100, blue]);
+    return rgb2hex([red, 100, blue, 180]);
   };
 
-  const [gData, setgData] = useState(
-    strains.map((e) => ({
+  const mapping = (array: any[]) => {
+    return array.map((e) => ({
       lat: e.lat,
       lng: e.lon,
       //新しいウイルスほど長い、細い
@@ -61,27 +61,20 @@ const GlobeDefault = () => {
       //新しい日付ほど赤い
       color: calColor(new Date(e.date)),
       label: "division:" + e.division + "\ndivergence:" + e.divergence,
-    }))
-  );
+    }));
+  };
+
+  const [gData, setgData] = useState(mapping(strains));
 
   useEffect(() => {
     setgData(
-      strains
-        .filter(
+      mapping(
+        strains.filter(
           (e) =>
             new Date(e.date).getFullYear() >= startYear &&
             new Date(e.date).getFullYear() <= endYear
         )
-        .map((e) => ({
-          lat: e.lat,
-          lng: e.lon,
-          //新しいウイルスほど長い、細い
-          size: Number(e.divergence) * 0.005,
-          r: 0.5 + 25 / Number(e.divergence),
-          //新しい日付ほど赤い
-          color: calColor(new Date(e.date)),
-          label: "division:" + e.division + "\ndivergence:" + e.divergence,
-        }))
+      )
     );
   }, [startYear, endYear]);
 
